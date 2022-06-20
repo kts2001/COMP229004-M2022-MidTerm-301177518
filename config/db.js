14077a5e8e1496e1d0a397aad9efe09ddfeb13ab
Kareem Zahra <kts2001@live.com>
2022-06-20 14:05:50 -0400

    MongoDB Atlas changed

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 // Do not expose your credentials in your code.
-let atlasDB = "mongodb+srv://<username>:<passoword>@<cluster>/carstore?retryWrites=true&w=majority";
+let atlasDB = "mongodb+srv://kts2001:kareem@cluster1.ysovm.mongodb.net/?retryWrites=true&w=majority";
 
 // Database setup
 let mongoose = require('mongoose');
